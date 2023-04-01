dafd049f5389ed537563ab858d190d3734857999
Muscaaa <chouiteknadjibmi@gmail.com>
2023-04-01 06:10:57 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React,{useEffect} from "react";
+
+import './App.css';
+import SearchIcon from './search.svg'
+
+const API_URL ='http://www.omdbapi.com?apikey=1f24baed';
 
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search)
+    }
+    useEffect(() => {
+        searchMovies('Spiderman');
+    },[])
     return(
-        <h1>App</h1>
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input 
+                    placeholder="Search For movies"
+                    value='Superman'
+                    onChange={() => {}}
+                />
+                <img src={SearchIcon} alt="search" onClick={() => {}}/>
+            </div>
+        </div>
     )
 }
 
